aa21b06ebc3153e2e5c629ee4925946f8fbf1cb7
Royyeungcm <122951000+Royyeungcm@users.noreply.github.com>
2023-06-08 14:29:10 -0400

    Initial Commit

---
+++ Project_Gp03/Project_Gp03App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_Gp03App.swift
+//  Project_Gp03
+//
+//  Created by Roy's Macbook on 8/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct Project_Gp03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
